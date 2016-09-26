1adeb182769ba6644417de2f23d66f2be396e5d6
Thom van Dijk <thom.a.vandijk@gmail.com>
2016-09-26 14:42:05 +0200

    added customvector2 class and gave needed variables to sheep

---
+++ Assets/scripts/CustomVector2.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CustomVector2 : MonoBehaviour {
+
+    // properties
+    public float x;
+    public float y;
+
+    // constructor
+    public CustomVector2(float xx, float yy) {
+        this.x = xx;
+        this.y = yy;
+    }
+
+    // methods
+    public CustomVector2 fromAngle(float angle) {
+        angle *= (Mathf.PI / 180);
+        this.x = Mathf.Cos(angle);
+        this.y = Mathf.Sin(angle);
+        return this;
+    }
+
+    public CustomVector2 copy() {
+        return new CustomVector2(this.x, this.y);
+    }
+
+    // info (getters)
+    public double mag() {
+        float getal = (x * x) + (y * y);
+        float pos = Mathf.Sqrt(getal);
+        return pos;
+    }
+
+    public double dist(CustomVector2 other) {
+        float p1 = this.x - other.x;
+        float p2 = this.y - other.y;
+        return Mathf.Sqrt(Mathf.Pow(p1, 2) + Mathf.Pow(p2, 2));
+    }
+
+    public CustomVector2 getNormalized() {
+        CustomVector2 t = new CustomVector2(this.x, this.y);
+        t.normalize();
+        return t;
+    }
+
+    public CustomVector2 normalize() {
+        float m = (float)this.mag();
+        this.x = this.x / m;
+        this.y = this.y / m;
+        return this;
+    }
+
+    public CustomVector2 limit(float max) {
+        if (this.mag() > max) {
+            this.normalize();
+            this.multS(max);
+        }
+        return this;
+    }
+
+    public float getAngle() {
+        float angle = Mathf.Atan2(y, x);
+        return angle * (180 / Mathf.PI);
+    }
+
+    // manipulators
+    public CustomVector2 add(CustomVector2 other) {
+        this.x += other.x;
+        this.y += other.y;
+        return this;
+    }
+
+    public CustomVector2 sub(CustomVector2 other) {
+        this.x -= other.x;
+        this.y -= other.y;
+        return this;
+    }
+
+    public CustomVector2 mult(CustomVector2 other) {
+        this.x *= other.x;
+        this.y *= other.y;
+        return this;
+    }
+
+    public CustomVector2 multS(float scalar) {
+        this.x *= scalar;
+        this.y *= scalar;
+        return this;
+    }
+
+    public CustomVector2 div(CustomVector2 other) {
+        this.x /= other.x;
+        this.y /= other.y;
+        return this;
+    }
+
+    public CustomVector2 divS(float scalar) {
+        this.x /= scalar;
+        this.y /= scalar;
+        return this;
+    }
+
+    public static float rad2deg(float a) {
+        return a * (180 / Mathf.PI);
+    }
+
+    public static float deg2rad(float a) {
+        return a * (Mathf.PI / 180);
+    }
+
+}
--- Assets/scripts/SheepBehaviour.cs
+++ Assets/scripts/SheepBehaviour.cs
@@ -4,25 +4,66 @@ using System.Collections;
 
 public class SheepBehaviour : MonoBehaviour {
 
-    //private List<GameObject> sheepList;
-    private Vector2 position;
+    private CustomVector2 position;
+    private CustomVector2 velocity;
+    private CustomVector2 acceleration;
+
+    private float maxforce;        //Maximum steering force.
+    private float maxspeed;        //Maximum speed.
+    private float neighbordist;     //Detection range.
+    private float separation;       //Separation between two boids.
+    private float rotation;
+
+    private int diameter;
 
     // Use this for initialization.
     void Start () {
-        //sheepList = new List<GameObject>();
-        position = new Vector2(this.transform.position.x, this.transform.position.y);
+        position = new CustomVector2(this.transform.position.x, this.transform.position.z);
+        velocity = new CustomVector2(0, 0);
+        acceleration = new CustomVector2(0, 0);
+
+        maxforce = 0.04f;
+        maxspeed = 3.0f;
+        neighbordist = 120;
+        separation = 40;
+
+        diameter = 32;
     }
 	
 	// Update is called once per frame.
 	void Update() {
-	
-	}
+        velocity.add(acceleration);
+
+        rotation = CustomVector2.deg2rad(velocity.getAngle());
+
+        velocity.limit(maxspeed);
+        position.add(velocity);
+
+        acceleration.multS(0);
+
+        this.transform.position = new Vector3(position.x, this.transform.position.y, position.y);
+    }
 
     // UpdateSheep is called from the FlockManager class.
     public void updateSheep(List<GameObject> sheepList) {
-        foreach (var t in sheepList) {
-            //Debug.Log(t.transform.position.x);
-        }
+        flock(sheepList);
+    }
+
+    // We accumulate a new acceleration each time based on three rules.
+    public void flock(List<GameObject> sheepList) {
+        //CustomVector2 sep = separate(sheepList);
+        //CustomVector2 ali = align(sheepList);
+        //CustomVector2 coh = cohesion(sheepList);
+
+        // Arbitrarily weight these forces.
+        //sep.multS(1.8f);
+        //ali.multS(1.0f);
+        //coh.multS(1.0f);
+
+        // Add the force vectors to acceleration.
+        //applyForce(sep);
+        //applyForce(ali);
+        //applyForce(coh);
     }
 
 }
